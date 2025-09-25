fd9896c75e001635c5ee2267dd71aeb1ab29a852
WSL HOME <raulrecrec@gmail.com>
2025-09-25 16:39:01 +0200

    Add initial philo.h with required libraries included

---
+++ philosophers.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 16:36:55 by rexposit          #+#    #+#             */
+/*   Updated: 2025/09/25 16:38:40 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>		// printf
+# include <stdlib.h>	// malloc, free
+# include <unistd.h>	// write, usleep
+# include <string.h>	// memset
+# include <sys/time.h>	// gettimeofday
+# include <pthread.h>	// threads & mutex
+
+
+
+#endif
