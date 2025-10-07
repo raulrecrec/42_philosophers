33524357ec42439f38a588a8f08f6d315cf4b336
WSL HOME <raulrecrec@gmail.com>
2025-10-07 21:18:32 +0200

    Add ph_atoll for strict positive number parsing
    * Skip leading whitespace and accept optional '+'; reject '-' as invalid.
    * Skip leading zeros and guard by length to avoid extreme inputs.
    * Convert digits-only sequence; reject any trailing non-digit characters.
    * Integrate ph_error wrapper to set ERR_ARGS on invalid input.
    * Return parsed value; caller is expected to enforce project policy (> 0) and int range if needed.

---
+++ utils/ph_atoll.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ph_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 17:30:03 by rexposit          #+#    #+#             */
+/*   Updated: 2025/10/07 21:18:25 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+long long	ph_atoll(const char *nptr, t_data *data)
+{
+	long long	res;
+
+	res = 0;
+	while (*nptr == ' ' || *nptr == '\t' || *nptr == '\n'
+		|| *nptr == '\v' || *nptr == '\f' || *nptr == '\r')
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			return(ph_error(data, ERR_ARGS));
+		nptr++;
+	}
+	while (*nptr == '0')
+		nptr++;
+	if (ph_strlen(nptr) > 20)
+		return(ph_error(data, ERR_ARGS));
+	while (ph_isdigit(*nptr))
+	{
+		res = res * 10 + (*nptr - '0');
+		nptr++;
+	}
+	if (*nptr != '\0' || !ph_isdigit(*nptr))
+		return(ph_error(data, ERR_ARGS));
+	return (res);
+}
