aaa0bdf6764e7c407ed09183820407782a5070e4
WSL HOME <raulrecrec@gmail.com>
2025-10-08 14:18:58 +0200

    parse: update ph_atoll length guard to ERR_RANGE - Treat excessive numeric length (after skipping leading zeros) as ERR_RANGE instead of ERR_ARGS. - Keep ERR_ARGS for invalid sign, missing digits, or trailing non-digit characters. - Preserve overall parsing flow: skip whitespace, optional '+', leading zeros, digits-only accumulation, and strict end-of-string.

--- utils/ph_atoll.c
+++ utils/ph_atoll.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 17:30:03 by rexposit          #+#    #+#             */
-/*   Updated: 2025/10/08 10:12:25 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/10/08 14:17:59 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ long long	ph_atoll(t_data *data, const char *nptr)
 	while (*nptr == '0')
 		nptr++;
 	if (ph_strlen(nptr) > 20)
-		return(ph_error(data, ERR_ARGS));
+		return(ph_error(data, ERR_RANGE));
 	while (ph_isdigit(*nptr))
 	{
 		res = res * 10 + (*nptr - '0');
