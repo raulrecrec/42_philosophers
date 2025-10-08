f68a5f19a47220e0030115eb9bdc824a3d762baa
WSL HOME <raulrecrec@gmail.com>
2025-10-08 10:42:21 +0200

    Refactor ph_malloc signature to take context first - Change function signature to 'void *ph_malloc(t_data *data, size_t size)' for consistency. - Set ERR_MALLOC via ph_error on allocation failure and return NULL. - Update call sites to pass 'data' as the first parameter.

--- utils/ph_malloc.c
+++ utils/ph_malloc.c
@@ -6,13 +6,13 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 17:29:50 by rexposit          #+#    #+#             */
-/*   Updated: 2025/10/07 21:26:27 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/10/08 10:41:40 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	*ph_malloc(size_t size, t_data *data)
+void	*ph_malloc(t_data *data, size_t size)
 {
 	void	*ptr;
 
