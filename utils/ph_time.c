4be044def7d2b5b5b7acaa1ab158f31b7d6b29ca
WSL HOME <raulrecrec@gmail.com>
2025-10-10 22:15:09 +0200

    Add ph_time.c with now_ms() utility - Create utils/ph_time.c as the time utilities module. - Implement now_ms() returning current time in milliseconds since epoch via gettimeofday. - Use 1000LL to force 64-bit arithmetic and avoid overflow during conversion. - Return -1 as a sentinel on gettimeofday failure to keep error semantics consistent. - Expose the prototype in philosophers.h and add utils/ph_time.c to SRCS in the Makefile.

---
+++ utils/ph_time.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ph_time.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 21:42:05 by rexposit          #+#    #+#             */
+/*   Updated: 2025/10/10 22:13:26 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+long long	now_ms(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) != 0)
+		return (-1);
+	return ((time.tv_sec * 1000LL) + (time.tv_usec / 1000LL));
+}
