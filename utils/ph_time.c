099a64a55fd8449d06eb75e3dea003ea03defc0e
WSL HOME <raulrecrec@gmail.com>
2025-10-11 04:08:19 +0200

    Implement smart_usleep(ms) with adaptive micro-sleeps and error handling - Sleep in short chunks while measuring elapsed time with now_ms(). - Use 1ms slices when remaining > 2ms and ~200µs for final calibration to improve precision. - Handle invalid inputs (ms <= 0) and exit early if now_ms() fails (start == -1 or elapsed == -1). - Keep all timing arithmetic in milliseconds and convert to microseconds only for usleep().

--- utils/ph_time.c
+++ utils/ph_time.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 21:42:05 by rexposit          #+#    #+#             */
-/*   Updated: 2025/10/10 22:41:09 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/10/11 04:01:21 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,27 @@ long long	now_ms(void)
 	if (gettimeofday(&time, NULL) != 0)
 		return (-1);
 	return ((time.tv_sec * 1000LL) + (time.tv_usec / 1000LL));
+}
+
+void	smart_usleep(long long ms)
+{
+	long long	start;
+	long long	elapsed;
+	long long	ms_left;
+
+	elapsed = 0;
+	start = now_ms();
+	if (ms <= 0 || start == -1)
+		return ;
+	while (elapsed < ms)
+	{
+		elapsed = now_ms() - start;
+		if (elapsed == -1)
+			return ;
+		ms_left = ms - elapsed;
+		if (ms_left > 2)
+			usleep(1000);
+		else
+			usleep(200);
+	}
 }
