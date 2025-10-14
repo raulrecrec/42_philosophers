6f8c1d56e77fa7c03f6e2c6ba1d68a6f6f99bc0f
WSL HOME <raulrecrec@gmail.com>
2025-10-14 22:32:52 +0200

    Add init_philos() to wire philosopher structures - Implement init_philos(t_data *data) in core/init_philos.c. - Set id as 1..N for logging clarity. - Assign left_hand = &forks[i] and right_hand = &forks[(i + 1) % N] to form ring topology. - Initialize times_eaten = 0 and last_meal_ms = 0; set back-reference philo->data = data. - Use local N cache and loop i < N for clarity and norm compliance.

--- core/init_philos.c
+++ core/init_philos.c
@@ -6,9 +6,29 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:09:18 by rexposit          #+#    #+#             */
-/*   Updated: 2025/10/14 22:09:26 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/10/14 22:32:34 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+void	init_philos(t_data *data)
+{
+	int		i;
+	int		n;
+	t_philo	*philo;
+
+	n = data->total_philos;
+	i = 0;
+	while (i < n)
+	{
+		philo = &data->philos[i];
+		philo->id = i + 1;
+		philo->left_hand = &data->forks[i];
+		philo->right_hand = &data->forks[(i + 1) % n];
+		philo->times_eaten = 0;
+		philo->last_meal_ms = 0;
+		philo->data = data;
+		i++;
+	}
+}
