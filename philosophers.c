e2e6996f2199b98be1a34790c387965890fc4ba8
WSL HOME <raulrecrec@gmail.com>
2025-10-23 01:07:22 +0200

    - Add temporary test main (non-final) to iterate quickly - Place under src/tests/main_tmp.c with clear TODO notes for later removal - Minimal CLI: parse args, run a few common scenarios, print timestamps/status - Wire optional build target 'philo_test' in Makefile (does not affect release binary) - Mark file to be excluded from Norminette/release builds until finalized

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 15:48:18 by rexposit          #+#    #+#             */
-/*   Updated: 2025/10/21 06:04:26 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/10/23 01:03:01 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ static void	data_set(t_data *data, int index, long long value)
 }
 
 static void
-	parser_and_validate_arguments(t_data *data, int argc, const char **argv)
+	parser_and_validate_arguments(t_data *data, int argc, char **argv)
 {
 	int			i;
 	long long	value;
@@ -70,7 +70,11 @@ static void
 
 int	main(int argc, char **argv)
 {
-	t_data	data;
+	t_data		data;
+	pthread_t	*threads;
+	pthread_t	monitor;
+	int			i;
+	int			j;
 
 	if (argc < 5 || argc > 6)
 	{
@@ -84,5 +88,69 @@ int	main(int argc, char **argv)
 	parser_and_validate_arguments(&data, argc, argv);
 	if (data.err != ERR_OK)
 		return (data.err);
-	return (0);
+	if (setup_alloc(&data) != ERR_OK)
+		return (data.err);
+	if (init_mutexes(&data) != ERR_OK)
+	{
+		free_arrays(&data);
+		return (data.err);
+	}
+	init_philos(&data);
+	data.time_start = now_ms();
+	i = 0;
+	while (i < data.total_philos)
+	{
+		pthread_mutex_lock(&data.m_last_meal_ms);
+		data.philos[i].last_meal_ms = data.time_start;
+		pthread_mutex_unlock(&data.m_last_meal_ms);
+		i++;
+	}
+	threads = ph_malloc(&data, data.total_philos * sizeof(pthread_t));
+	if (threads == NULL)
+	{
+		destroy_forks(&data);
+		destroy_globals(&data);
+		free_arrays(&data);
+		return (data.err);
+	}
+	i = 0;
+	while (i < data.total_philos)
+	{
+		if (pthread_create(&threads[i], NULL, philo_routine, &data.philos[i]) != 0)
+		{
+			ph_error(&data, ERR_THREAD_CREATE);
+			set_dead(&data, 1);
+			j = i - 1;
+			while (j >= 0)
+			{
+				pthread_join(threads[j], NULL);
+				j--;
+			}
+			destroy_forks(&data);
+			destroy_globals(&data);
+			free_arrays(&data);
+			free(threads);
+			return (data.err);
+		}
+		i++;
+	}
+	if (pthread_create(&monitor, NULL, monitor_routine, &data) != 0)
+	{
+		ph_error(&data, ERR_THREAD_CREATE);
+		set_dead(&data, 1);
+		i = 0;
+		while (i < data.total_philos)
+		{
+			pthread_join(threads[i], NULL);
+			i++;
+		}
+		destroy_forks(&data);
+		destroy_globals(&data);
+		free_arrays(&data);
+		free(threads);
+		return (data.err);
+	}
+	cleanup_after_sim(&data, threads, monitor);
+	free(threads);
+	return (data.err);
 }
