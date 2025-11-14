3a68ec0811ef8ff4f8df16f4e3932b80f2dbc2b7
WSL HOME <raulrecrec@gmail.com>
2025-11-14 22:13:16 +0100

    Update philosophers.c to separate initialization and simulation control
    - Added init_program to handle argument validation, data initialization, memory allocation, mutex setup and philosopher initialization.
    - Set data.time_start inside init_program after successful initialization.
    - Simplified main to call init_program and create_threads_and_monitor, then run cleanup_after_sim and free the threads array before returning data.err.

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 15:48:18 by rexposit          #+#    #+#             */
-/*   Updated: 2025/10/23 01:03:01 by rexposit         ###   ########.fr       */
+/*   Updated: 2025/11/14 21:55:53 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,88 +68,43 @@ static void
 	}
 }
 
-int	main(int argc, char **argv)
+static int	init_program(t_data *data, int argc, char **argv)
 {
-	t_data		data;
-	pthread_t	*threads;
-	pthread_t	monitor;
-	int			i;
-	int			j;
-
 	if (argc < 5 || argc > 6)
 	{
 		printf("Invalid arguments.\nCorrect format "
 			"(last one is optional and only positive integers > 0): "
 			"<number_of_philosophers> <time_to_die> <time_to_eat> "
 			"<time_to_sleep> [number_of_times_each_philosopher_must_eat]\n");
-		return (ERR_ARGS);
+		data->err = ERR_ARGS;
+		return (data->err);
 	}
-	data_init(&data);
-	parser_and_validate_arguments(&data, argc, argv);
-	if (data.err != ERR_OK)
-		return (data.err);
-	if (setup_alloc(&data) != ERR_OK)
-		return (data.err);
-	if (init_mutexes(&data) != ERR_OK)
+	data_init(data);
+	parser_and_validate_arguments(data, argc, argv);
+	if (data->err != ERR_OK)
+		return (data->err);
+	if (setup_alloc(data) != ERR_OK)
+		return (data->err);
+	if (init_mutexes(data) != ERR_OK)
 	{
-		free_arrays(&data);
+		free_arrays(data);
+		return (data->err);
+	}
+	init_philos(data);
+	data->time_start = now_ms();
+	return (data->err);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data		data;
+	pthread_t	*threads;
+	pthread_t	monitor;
+
+	if (init_program(&data, argc, argv) != ERR_OK)
 		return (data.err);
-	}
-	init_philos(&data);
-	data.time_start = now_ms();
-	i = 0;
-	while (i < data.total_philos)
-	{
-		pthread_mutex_lock(&data.m_last_meal_ms);
-		data.philos[i].last_meal_ms = data.time_start;
-		pthread_mutex_unlock(&data.m_last_meal_ms);
-		i++;
-	}
-	threads = ph_malloc(&data, data.total_philos * sizeof(pthread_t));
-	if (threads == NULL)
-	{
-		destroy_forks(&data);
-		destroy_globals(&data);
-		free_arrays(&data);
+	if (create_threads_and_monitor(&data, &threads, &monitor) != ERR_OK)
 		return (data.err);
-	}
-	i = 0;
-	while (i < data.total_philos)
-	{
-		if (pthread_create(&threads[i], NULL, philo_routine, &data.philos[i]) != 0)
-		{
-			ph_error(&data, ERR_THREAD_CREATE);
-			set_dead(&data, 1);
-			j = i - 1;
-			while (j >= 0)
-			{
-				pthread_join(threads[j], NULL);
-				j--;
-			}
-			destroy_forks(&data);
-			destroy_globals(&data);
-			free_arrays(&data);
-			free(threads);
-			return (data.err);
-		}
-		i++;
-	}
-	if (pthread_create(&monitor, NULL, monitor_routine, &data) != 0)
-	{
-		ph_error(&data, ERR_THREAD_CREATE);
-		set_dead(&data, 1);
-		i = 0;
-		while (i < data.total_philos)
-		{
-			pthread_join(threads[i], NULL);
-			i++;
-		}
-		destroy_forks(&data);
-		destroy_globals(&data);
-		free_arrays(&data);
-		free(threads);
-		return (data.err);
-	}
 	cleanup_after_sim(&data, threads, monitor);
 	free(threads);
 	return (data.err);
